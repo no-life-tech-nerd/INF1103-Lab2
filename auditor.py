9216be3e048074f19d22c3329e22bb8dfd450ac8
no-life-tech-nerd <2604171@sit.singaporetech.edu.sg>
2026-09-11 20:25:53 +0800

    feat(auditor): step 3 + 4, input type validation & handling

--- auditor.py
+++ auditor.py
@@ -9,3 +9,8 @@ while userInput != "quit":
     if userInput == "quit":
         print("Goodbye!")
         break
+    try:
+        addedInventory = int(userInput)
+    except:
+        warnUser(f"Input of type {str(type(userInput))} not accepted, try again!")
+        continue

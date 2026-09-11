888aa77afcdb25a2f36a3da5da26409e54b869b9
no-life-tech-nerd <2604171@sit.singaporetech.edu.sg>
2026-09-11 20:25:53 +0800

    feat(auditor): step 2, loop input till quit

--- auditor.py
+++ auditor.py
@@ -1,1 +1,11 @@
+from logging import warning
+
 inventory = int(0)
+
+userInput = ""
+
+while userInput != "quit":
+    userInput = input("Enter a stock quantity: ")
+    if userInput == "quit":
+        print("Goodbye!")
+        break
